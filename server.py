6603f3e8e545444f346093ec3c8419497134cc5a
karansondh <63238367+karansondh@users.noreply.github.com>
2020-06-25 22:20:50 +0530

    my portfolio website

--- server.py
+++ server.py
@@ -14,15 +14,6 @@ def other_comps(file_name):
     return render_template(file_name)
 
 
-def write_to_file(data):
-    with open('database.txt', mode='a') as database:
-        email = data['email']
-        subject = data['subject']
-        message = data['message']
-
-        file = database.write(f'\n{email},{subject},{message}')
-
-
 def write_to_csv(data):
     with open('database.csv', newline='', mode='a') as database2:
         email = data['email']
